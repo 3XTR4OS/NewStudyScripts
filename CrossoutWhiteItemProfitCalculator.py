ad9d4e3c0a1e0866aa2174188fd89b977e2d59f4
Ragulin Eugene <84981999+3XTR4OS@users.noreply.github.com>
2022-11-20 09:53:27 +1000

    v2.0

--- CrossoutWhiteItemProfitCalculator.py
+++ CrossoutWhiteItemProfitCalculator.py
@@ -1,14 +1,33 @@
-item_price = float(input('Цена предмета: '))
+def craft_price(item):
+    weapons = ('пул', 'пуш', 'дроб', "пулемёт", "дробовик", "пушка")
+    cabs = ('каб', 'кабина')
+    truster = ('уск', "ускор", "ускоритель")
+    fuel = ('бак', "бочка")
+    radar = ('радар', "радар", "рубин")
+    module = ("радио", "радик", "радиатор", "домкрат")
+    wheels = ('колесо', "колёса")
 
-scrap_price = float(input('Цена металла:'))
-scrap_count = float(input('Кол-во металла для крафта:'))
+    white_item_crafts = {
+        cabs: (75, 15),
+        weapons: (30, 6),
+        truster: (55, 11),
+        fuel: (60, 12),
+        radar: (35, 8),
+        module: (20, 4),
+        wheels: (15, 3)
+    }
 
-cuprum_price = float(input('Цена на медь:'))
-cuprum_count = float(input('Кол-во меди для крафта:'))
+    for item_set in white_item_crafts:
+        if item in item_set:
+            craft_needs = white_item_crafts[item_set]  # (scrap, cuprum)
 
-scrap_counter = (scrap_price * 0.01) * scrap_count
-cuprum_counter = (cuprum_price * 0.01) * cuprum_count
+            white_item_price = float(input('Цена белого предмета: '))
+            scrap_price = (float(input('Цена металла: ')) * 0.01) * craft_needs[0]
+            cuprum_price = (float(input('Цена меди: ')) * 0.01) * craft_needs[-1]
 
-print('Цена предмета =', item_price)
-print('Цена ресурсов =', scrap_counter + cuprum_counter)
-print('Разница:', item_price - (scrap_counter + cuprum_counter))
+            print('Цена предмета = ', white_item_price)
+            print('Цена ресурсов = ', scrap_price + cuprum_price)
+
+
+craft_price(input('Деталь: '))
+
